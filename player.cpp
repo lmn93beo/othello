24c7873dc12c8a1c2294707f9a46f4e7c72b032e
Nhat <lmn93beo@yahoo.com>
2016-03-02 14:13:09 -0800

    Changed NULL to player2 move

--- player.cpp
+++ player.cpp
@@ -39,5 +39,5 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */ 
-    return NULL;
+    return opponentsMove;
 }
